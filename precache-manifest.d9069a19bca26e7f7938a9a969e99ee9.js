487a6d9e8f055b3594146af0c20f7c5899302c13
Abiya Yaccobu <abiyay30@gmail.com>
2020-08-03 23:20:09 +0100

    Updates

---
+++ precache-manifest.d9069a19bca26e7f7938a9a969e99ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523c1e057849baf1e76b5e41703bec1b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ee64392748506b6db756",
+    "url": "/weather/static/css/main.ca2301d5.chunk.css"
+  },
+  {
+    "revision": "ea0b0c685ebd4ade92dc",
+    "url": "/weather/static/js/2.9718143a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.9718143a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee64392748506b6db756",
+    "url": "/weather/static/js/main.dd3bf26f.chunk.js"
+  },
+  {
+    "revision": "21baee1ee3fded119a76",
+    "url": "/weather/static/js/runtime-main.6859beea.js"
+  }
+]);
